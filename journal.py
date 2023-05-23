1f714481646d35aca387945a088a14f5220c2299
AndedeaK <64412996+AndedeaK@users.noreply.github.com>
2023-05-23 19:56:39 +0200

    Add files via upload
    Here we are now writing text to a text file. I have the idea of instead maybe using a sqlite database to store entries or before that a dictionary. This is who it is easier to keep
    track of entries. Count them and so forth.

--- journal.py
+++ journal.py
@@ -16,12 +16,31 @@ day  = int(day)
 
 d = date(year, month, day)
 
-entry = input("You may make your new entry: ")
+# Cast the date to str for it to be written
+date = str(d)
+
+# Let us write the date to the text file with a new line
+file1 = open("text", "a")
+
+# Add the date to the text file
+# You might want to consider if it is the first entry or not for knowing when to write new lines.
+# This new line is just for looks
+file1.write("\n \n")
+
+file1.write(date)
+file1.write("\n \n")
+entry = [input("you may now make your entry: ")]
+file1.writelines(entry)
+
+# Remember to also close the file
+file1.close
+
+# entry = input("You may make your new entry: ")
 
 # Now display the entry with date and time
-print(d)
+""" print(d)
 print(entry)
-print("Would you like to save this entry?: y/n_")
+print("Would you like to save this entry?: y/n_") """
 
 
 """ d = date(2023, 5, 23)
