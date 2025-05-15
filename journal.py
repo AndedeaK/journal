1d28d655bf64cd4a824b849672eb2976f5edfb48
AndedeaK <agkouter@gmail.com>
2025-05-15 06:27:17 +0200

    Added simplifid date and writing and storing.

--- journal.py
+++ journal.py
@@ -1,28 +1,32 @@
-# Import datetime date module
+""" Unstructured journalling is free writing without and pre-sets. I.E "What are your grateful for?" already written in
+the journal. You just write freely. 
+According to https://en.wikipedia.org/wiki/Diary the word 'journal' and 'diary' are often used interchangeably. But
+a 'diary' aims to have daily entries whereas a 'journal' entries can be done less often. """
+
+"""This is a command-line journal to improve privacy and to provide a feeling ease when writing. You can just relax and
+write freely as this is an unstructured journal."""
+
+# import libraries
+from argparse import ArgumentParser
 from datetime import date
 
+date = date.today()
 
-# Make a new journal entry (python3 journal.py new)
-
-    # Firstly, enter the date
-
-# Get the date from the user
-year, month, day = input("Please enter the date: ").split("-")
-
-# change the date from string to int
-year = int(year)
-month = int(month)
-day  = int(day)
-
-d = date(year, month, day)
-
-entry = input("You may make your new entry: ")
-
-# Now display the entry with date and time
-print(d)
-print(entry)
-print("Would you like to save this entry?: y/n_")
+parser = ArgumentParser("A Command line journal")
+parser.add_argument("-n", "--new", help="Make a new journal entry", action="store_true")
+parser.add_argument("-v", "--view", help="To view your journal entries", action="store_true")
+args = parser.parse_args()
 
 
-""" d = date(2023, 5, 23)
-print(type(d.year)) """
+if args.new:
+    entry = input("Feel free to make your entry: ")
+    with open("journal.txt", "a") as file:
+        file.write(f"{date}\nDear journal\n{entry}\n")
+elif args.view:
+    with open("journal.txt", "r") as file:
+        for line in file:
+            print(line.rstrip())
+    
+
+
+# Create the apps help menu with -h
