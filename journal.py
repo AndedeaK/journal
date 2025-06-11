17c6751e46687ac060af64220dc58ab9dfbb1f30
AndedeaK <agkouter@gmail.com>
2025-06-11 14:25:52 +0200

    introduced functions and test functions successfully

--- journal.py
+++ journal.py
@@ -1,8 +1,7 @@
-"""Unstructured journalling is free writing without and pre-sets. I.E "What are your grateful for?" already written in
-the journal. You just write freely.
+""" Unstructured journalling is free writing without and pre-sets. I.E "What are your grateful for?" already written in
+the journal. You just write freely. 
 According to https://en.wikipedia.org/wiki/Diary the word 'journal' and 'diary' are often used interchangeably. But
-a 'diary' aims to have daily entries whereas a 'journal' entries can be done less often.
-"""
+a 'diary' aims to have daily entries whereas a 'journal' entries can be done less often. """
 
 """This is a command-line journal to improve privacy and to provide a feeling ease when writing. You can just relax and
 write freely as this is an unstructured journal."""
@@ -12,41 +11,46 @@ from argparse import ArgumentParser
 from datetime import date
 import sys
 
+# Assist the user to use the programme
+parser = ArgumentParser("A Command line journal")
+parser.add_argument("-n", "--new", help="Make a new journal entry", action="store_true")
+parser.add_argument("-v", "--view", help="To view your journal entries", action="store_true")
+args = parser.parse_args()
+
 # Today's date
 date = date.today()
 
-parser = ArgumentParser("A Command line journal")
-parser.add_argument("-n", "--new", help="Make a new journal entry", action="store_true")
-parser.add_argument(
-    "-v", "--view", help="To view all your journal entries", action="store_true"
-)
-args = parser.parse_args()
-
 
 def main():
-    # Add some error checking on the command line
-    if len(sys.argv) < 2:
-        sys.exit("Too few arguments. Type -h or --help for assistance")
-
-    # Parse the command-line
-    if args.new:
-        entry = input("Feel free to make your entry: ")
-        add(entry)
-        
-    elif args.view:
-        with open("journal.txt", "r") as file:
-            for line in file:
-                print(line.rstrip())
-
-# Adds a new entry to the journal and shows it to the user
-def add(entry):
-    
-    with open("journal.txt", "a") as file:
-        file.write(f"{date}\nDear journal\n{entry}\n")
-
-
+	# Further assist the user on the commandline
+	check_cmdline()
+	
+	# Execute the user's disire
+	if args.new:
+    		entry = input("Feel free to make your entry: ")        		
+    		print(make_entry(entry))    		
+	elif args.view:
+    		with open("journal.txt", "r") as file:
+        		for line in file:
+            			print(line.rstrip())
+            			
+def check_cmdline():
+	if len(sys.argv) < 2:
+		sys.exit("Too few arguments. Type{python3 journal.py -h] for help}")
+            			
+            			
+def make_entry(entry: str) -> None:
+	if isinstance(entry, int) == True:
+		raise TypeError("Your entry must be a str.")
+	if entry.isdigit():
+		raise ValueError("Those are only digits.")
+	else:
+		with open("journal.txt", "a") as file:
+        		file.write(f"{date}\nDear journal\n{entry}\n")
+        		return "Your entry as been entered into your yournal successfully."
+       
+	      
+        	
+            		
 if __name__ == "__main__":
-    main()
-
-
-# Create the apps help menu with -h
+	main()
